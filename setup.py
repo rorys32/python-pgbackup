cb4a9cf2e91b8216f39ce51a3f66fdda6b18bda7
rorys32 <roryrs@gmail.com>
2018-05-17 13:59:49 -0400

    adding the last of the pgbackup utility

--- setup.py
+++ setup.py
@@ -9,7 +9,12 @@ setup(
     description='Database backups locally or to AWS S3.',
     long_description=readme,
     author='Rory',
+    install_requires=['boto3'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[]
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+            ]
+        }
 )
